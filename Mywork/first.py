48f93862507b4ee978331553487194f03eaa4922
oscarhu19 <129806819+oscarhu19@users.noreply.github.com>
2023-12-11 20:51:47 -0600

    Add files via upload

---
+++ Mywork/first.py
@@ -0,0 +1,20 @@
+# Assign values to variables
+a = 10
+b = 3
+c= 1
+# Some operations
+sum_result = a + b + c
+diff_result = a - b - c
+prod_result = a * b * c
+div_result = a / b / c
+div2_result = a // b // c
+div3_result = a // (b / c)
+
+# Print results
+print("value of a: ", a, "value of b: ", b, "value of c: ", c)
+print("Sum: ", sum_result)
+print("Difference: ", diff_result)
+print("Product: ", prod_result)
+print("Division: ", div_result)
+print("Integer Division1: ", div2_result)
+print("Integer Division2: ", div3_result)
